23cd2174fb023fa05cd57cbe8435f6a1dda0cd3d
Nur Alam Shawon <ahsan.shawon9@gmail.com>
2025-06-26 20:50:07 +0600

    herosection carosel part done

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const HeroSection = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Carousel infiniteLoop={true} 
+    autoPlay={true} transitionTime={1000} 
+    showThumbs={false} 
+    showArrows={false} 
+    showStatus={false} 
+    showIndicators={false}
+    stopOnHover={false}
+    className="my-10">
+      <div>
+        <img src="https://i.postimg.cc/qBbTCKZC/banner1.png" />
+      </div>
+      <div>
+        <img src="https://i.postimg.cc/7hWr8vTN/banner2.png" />
+      </div>
+      <div>
+        <img src="https://i.postimg.cc/sDMRP2tH/banner3.png" />
+      </div>
+    </Carousel>
+  );
 };
 
 export default HeroSection;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import HeroSection from "../Components/HeroSection/HeroSection";
+import SecondSection from "../Components/SecondSection/SecondSection";
+import ThirdSection from "../Components/ThirdSection/ThirdSection";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <HeroSection></HeroSection>
+      <SecondSection></SecondSection>
+      <ThirdSection></ThirdSection>
+    </div>
+  );
 };
 
 export default Home;
